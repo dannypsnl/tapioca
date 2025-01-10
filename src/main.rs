8ba00a2c0c644171b3b6f15a5f68202d90c1e2aa
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-10 16:01:15 +0800

    ast
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use chumsky::prelude::*;
+use enotation::ENotation;
+
+pub struct Module {
+    claim_forms: Vec<ClaimForm>,
+    define_forms: Vec<DefineForm>,
+    other_forms: Vec<ENotation>,
+}
+
+pub struct ClaimForm {
+    // (: x : int)
+    // claims `x` has type `int`
+    id: String,
+    // TODO: normalize this to internal AST
+    typ: ENotation,
+}
+
+// NOTE: (define x : <type> <expr>) will be elaborated to
+// (: x : <type>)
+// (define x <expr>)
+pub enum DefineForm {
+    // (define x <expr>)
+    DefineConstant {
+        id: String,
+        expr: ENotation,
+    },
+    // (define (f x y z ...)
+    //   <body_1>
+    //   ...
+    //   <body_k>)
+    DefineFunction {
+        id: String,
+        params: Vec<String>,
+        body: Vec<ENotation>,
+    },
+}
+
 fn main() {
     println!("Hello, world!");
 }
