760ebf8e8f67cfe1ff5e51cfb03cc2e10dfa87d9
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-15 21:39:08 +0800

    test another case
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/expander/tests.rs
+++ src/expander/tests.rs
@@ -18,18 +18,22 @@ impl Display for ExprBody {
     }
 }
 
-#[test]
-fn expander() {
-    let source: (String, Source<String>) = ("test.ss".to_string(), Source::from("".to_string()));
-    let mut module = Module::new(source);
-
-    let mut ex = Expander {
+fn test_expander(module: &mut Module) -> Expander<'_> {
+    Expander {
         source: module.source.clone(),
-        module: &mut module,
+        module: module,
         rename_mapping: Default::default(),
         let_count: 0,
         lambda_count: 0,
-    };
+    }
+}
+
+#[test]
+fn expander_1() {
+    let source: (String, Source<String>) = ("test.ss".to_string(), Source::from("".to_string()));
+    let mut module = Module::new(source);
+    let mut ex = test_expander(&mut module);
+
     let mut scopes = HashSet::new();
     ex.insert(&"b".to_string(), scopes.clone(), "b1".to_string());
     let outer_scopes = scopes.clone();
@@ -40,4 +44,20 @@ fn expander() {
     ex.insert(&name, scopes.clone(), "a2".to_string());
     assert_snapshot!(ex.lookup_newname(&name, scopes.clone()), @"{origin: a, bind: a2}");
     assert_snapshot!(ex.lookup_newname(&"b".to_string(), outer_scopes.clone()), @"{origin: b, bind: b1}");
+}
+
+#[test]
+fn expander_2() {
+    let source: (String, Source<String>) = ("test.ss".to_string(), Source::from("".to_string()));
+    let mut module = Module::new(source);
+    let mut ex = test_expander(&mut module);
+
+    let mut scopes = HashSet::new();
+    scopes.insert(Scope::Let(0));
+    let outer_scopes = scopes.clone();
+    let name = "a".to_string();
+    ex.insert(&name, scopes.clone(), "a1".to_string());
+    scopes.insert(Scope::Let(1));
+    ex.insert(&name, scopes.clone(), "a2".to_string());
+    assert_snapshot!(ex.lookup_newname(&name, outer_scopes.clone()), @"{origin: a, bind: a1}");
 }
