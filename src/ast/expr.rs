8680a2fd8192f8b5a29128ec856583875d5aaa1b
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-16 16:08:26 +0800

    explain the usage of each identifier method
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/ast/expr.rs
+++ src/ast/expr.rs
@@ -39,18 +39,37 @@ pub enum Identifier {
 }
 
 impl Identifier {
-    // construction
+    /// Constructs a name that not been renamed, usually they are
+    /// 1. top-level definition
+    /// 2. parameters in top-level `(define (f ……) ……)` form
     pub fn top_level(name: String) -> Self {
         Identifier::TopLevel(name)
     }
 
-    // access
+    /// Access to the programmer wrote done name, for example
+    ///
+    /// (let ([x 1])
+    ///   (let ([x x])
+    ///     x))
+    ///
+    /// After expansion process, **enotation** been converted to internal AST, and renaming is complete
+    /// 1. The first `let x` will be renamed to, e.g. `x1`
+    /// 2. The second `let x` will be renamed to, e.g. `x2`
+    ///
+    /// Therefore, the renamed AST is
+    ///
+    /// (let ([x1 1])
+    ///   (let ([x2 x1])
+    ///     x2))
+    ///
+    /// But for reporting, shouldn't report cannot find `x2` at all, because no programmers expect the error message is talking internal name from compiler.
     pub fn info_name(&self) -> String {
         match self {
             Identifier::Bind { origin_name, .. } => origin_name.clone(),
             Identifier::TopLevel(origin_name) => origin_name.clone(),
         }
     }
+    /// The internal name that use to lookup in type system (after expansion)
     pub fn lookup_name(&self) -> &String {
         match self {
             Identifier::Bind { lookup_name, .. } => lookup_name,
