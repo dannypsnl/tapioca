d48652f9bc763a067aa7fd8933fd5c0ef8366924
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-18 12:32:11 +0800

    use s2c
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,18 +1,18 @@
+use crate::ast::expr::ExprBody;
 use crate::ast::typ::{self, TypBody};
 use crate::ast::{self, DefineForm};
 use crate::type_system::environment::Environment;
+use s2c::c_cg::c_stmt;
+use s2c::c_cg::c_type::CType;
+use s2c::c_cg::c_value::{CLiteral, CValue, IntegerSuffix};
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
-use tinyc::{CExpr, CFile, CStmt, Declare, DefineFunc};
-
-pub mod tinyc;
 
 struct Driver<'a> {
     module_path: &'a Path,
     module_name: String,
-    init_list: Vec<(String, CExpr)>,
-    cfile: CFile,
+    c_ctx: c_stmt::Context,
 }
 
 pub fn compile(root_path: &Path, env: &Environment<'_>, module: &ast::Module) {
@@ -26,10 +26,9 @@ pub fn compile(root_path: &Path, env: &Environment<'_>, module: &ast::Module) {
         .to_string();
 
     let mut driver = Driver {
+        c_ctx: c_stmt::Context::standard(module_name.clone()),
         module_path,
         module_name,
-        init_list: vec![],
-        cfile: Default::default(),
     };
 
     for def in &module.define_forms {
@@ -41,7 +40,7 @@ pub fn compile(root_path: &Path, env: &Environment<'_>, module: &ast::Module) {
     let output = Path::new("_build");
     std::fs::create_dir_all(output).expect("failed to create output directory");
     let mut f = File::create_buffered(output.join(cpath)).expect("failed to open output file");
-    write!(&mut f, "{}", driver.cfile).expect("failed to write");
+    write!(&mut f, "{}", driver.c_ctx.current_source.lock().unwrap()).expect("failed to write");
 
     let mut f = File::create_buffered(output.join(symbols)).expect("failed to open symbols file");
     write!(&mut f, "{}", env).expect("failed to write");
@@ -53,11 +52,9 @@ impl<'a> Driver<'a> {
             DefineForm::DefineConstant { span, id, expr } => {
                 let ty = env.lookup(id, span);
                 let name = self.mangle_name(&id.lookup_name());
-                self.cfile.declares.push(Declare {
-                    name: name.clone(),
-                    typ: self.convert_type(ty),
-                });
-                self.init_list.push((name, self.convert_expr(expr)));
+
+                self.c_ctx
+                    .set(self.convert_type(ty), name, self.convert_expr(expr));
             }
             DefineForm::DefineFunction {
                 span,
@@ -70,16 +67,38 @@ impl<'a> Driver<'a> {
                     result,
                 } = &env.lookup(id, span).body
                 {
-                    self.cfile.funcs.push(DefineFunc {
-                        name: self.mangle_name(&id.lookup_name()),
-                        params: params
-                            .iter()
-                            .map(|p| p.lookup_name())
-                            .zip(ptys.iter().map(|t| self.convert_type(t)))
+                    self.c_ctx.def(
+                        id.lookup_name(),
+                        self.convert_type(&result),
+                        ptys.iter()
+                            .map(|t| self.convert_type(t))
+                            .zip(params.iter().map(|p| p.lookup_name()))
                             .collect(),
-                        result: self.convert_type(&result),
-                        statement: self.convert_cstmt(body),
-                    });
+                        |ctx| {
+                            match &body.body {
+                                ExprBody::Begin(mid, last) => {
+                                    for m in mid {
+                                        self.convert_expr(m);
+                                    }
+                                    self.convert_expr(&last);
+                                }
+                                ExprBody::Let(bindings, expr) => {
+                                    for bind in bindings {
+                                        ctx.set(
+                                            self.convert_type(env.lookup(&bind.name, span)),
+                                            bind.name.lookup_name(),
+                                            self.convert_expr(&bind.expr),
+                                        );
+                                    }
+                                    self.convert_expr(expr);
+                                }
+                                _ => {
+                                    todo!()
+                                }
+                            }
+                            ctx
+                        },
+                    );
                 } else {
                     panic!("internal error: a function has no function type")
                 }
@@ -91,54 +110,26 @@ impl<'a> Driver<'a> {
         format!("{}__{}", self.module_name, name)
     }
 
-    fn convert_cstmt(&self, expr: &ast::expr::Expr) -> tinyc::CStmt {
-        match &expr.body {
-            ast::expr::ExprBody::Begin(es, expr) => {
-                let mut next = self.convert_cstmt(expr);
-                let mut eit = es.iter().rev();
-                while let Some(e) = eit.next() {
-                    next = CStmt::Seq {
-                        cur: self.convert_expr(e),
-                        next: Box::new(next),
-                    }
-                }
-                next
-            }
-            ast::expr::ExprBody::Let(binds, expr) => {
-                let mut next = self.convert_cstmt(expr);
-                let mut bit = binds.iter().rev();
-                while let Some(b) = bit.next() {
-                    next = CStmt::Assign {
-                        name: b.name.lookup_name().clone(),
-                        expr: self.convert_expr(&b.expr),
-                        next: Box::new(next),
-                    }
-                }
-                next
-            }
-
-            _ => CStmt::Return(self.convert_expr(expr)),
-        }
-    }
-
-    fn convert_type(&self, ty: &typ::Typ) -> tinyc::CTyp {
+    fn convert_type(&self, ty: &typ::Typ) -> CType {
         match &ty.body {
             // stdbool.h
-            TypBody::Bool => "bool".into(),
-            TypBody::Char => "char".into(),
-            TypBody::String => "tapi_string".into(),
+            TypBody::Bool => CType::I8,
+            TypBody::Char => CType::U8,
 
-            TypBody::Int => "int".into(),
-            // stdint.h
-            TypBody::I8 => "int8_t".into(),
-            TypBody::I16 => "int16_t".into(),
-            TypBody::I32 => "int32_t".into(),
-            TypBody::I64 => "int64_t".into(),
-            TypBody::U8 => "uint8_t".into(),
-            TypBody::U16 => "uint16_t".into(),
-            TypBody::U32 => "uint32_t".into(),
-            TypBody::U64 => "uint64_t".into(),
-            TypBody::Void => "void".into(),
+            // TODO: these need runtime supports
+            TypBody::String => todo!(),
+            TypBody::Int => todo!(),
+
+            // rely s2c
+            TypBody::I8 => CType::I8,
+            TypBody::I16 => CType::I16,
+            TypBody::I32 => CType::I32,
+            TypBody::I64 => CType::I64,
+            TypBody::U8 => CType::U8,
+            TypBody::U16 => CType::U16,
+            TypBody::U32 => CType::U32,
+            TypBody::U64 => CType::U64,
+            TypBody::Void => CType::Void,
 
             TypBody::Symbol => todo!(),
             TypBody::Rational => todo!(),
@@ -155,15 +146,17 @@ impl<'a> Driver<'a> {
         }
     }
 
-    fn convert_expr(&self, expr: &ast::expr::Expr) -> CExpr {
+    fn convert_expr(&self, expr: &ast::expr::Expr) -> CValue {
         match &expr.body {
             ast::expr::ExprBody::Bool(_) => todo!(),
             ast::expr::ExprBody::Char(_) => todo!(),
             ast::expr::ExprBody::String(_) => todo!(),
             ast::expr::ExprBody::Rational(_, _) => todo!(),
             ast::expr::ExprBody::Float(_) => todo!(),
-            ast::expr::ExprBody::Int(i) => CExpr::CInt(*i),
-            ast::expr::ExprBody::Identifier(id) => CExpr::Id(id.lookup_name().clone()),
+            ast::expr::ExprBody::Int(i) => {
+                CValue::Literal(CLiteral::Int(*i as usize, IntegerSuffix::I64))
+            }
+            ast::expr::ExprBody::Identifier(id) => CValue::Variable(id.lookup_name().clone()),
             ast::expr::ExprBody::Symbol(_) => todo!(),
             _ => todo!(),
         }
