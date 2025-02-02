557f050c758fe303ab9fba3a79c9d4610c938433
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-02-02 23:06:12 +0800

    expand import
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- lib/expander/Expander.ml
+++ lib/expander/Expander.ml
@@ -1,4 +1,23 @@
 open Tapioca_enotation.ENotation
 
+exception BadImport
+exception BadForm of notation
+
+type top = Import of string list
+
 let expand_file (_ns : notation list) = ()
-let expand_top (_n : notation) = ()
+
+let expand_top (n : notation) =
+  match n with
+  | L (Id "import" :: modules) ->
+    let modules =
+      List.map
+        (fun n ->
+           match n with
+           | Id n -> n
+           | _ -> raise BadImport)
+        modules
+    in
+    Import modules
+  | _ -> raise @@ BadForm n
+;;
