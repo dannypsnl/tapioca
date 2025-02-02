8dee0ec12f548620445b798a24f574f2410837ac
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-02-02 13:49:21 +0800

    fix ocamlformat version
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- lib/enotation/Parser.ml
+++ lib/enotation/Parser.ml
@@ -1,30 +1,24 @@
-exception UnexpectedToken of {
-  loc: Asai.Range.t
-}
+exception UnexpectedToken of { loc : Asai.Range.t }
 
-let rec enotation () : ENotation.t = 
+let rec enotation () : ENotation.t =
   let open Combinator in
   let open ENotation in
   let tok = next_token () in
   let loc = Option.get tok.loc in
-  match tok.value with 
-    | IDENTIFIER s -> Asai.Range.locate loc @@ Id s
-    | OPEN_PAREN ->
-      let notations = Combinator.many enotation () in
-      consume Lexer.CLOSE_PAREN;
-      Asai.Range.locate loc @@ L notations
-    | NOTATION_COMMENT ->
-      (* a #; comment will ignore next enotation, and take the next next enotaion *)
-      let _ = enotation () in
-      enotation ()
-    | CLOSE_PAREN
-    | EOF ->
-      raise (UnexpectedToken {
-        loc = loc
-      })
+  match tok.value with
+  | IDENTIFIER s -> Asai.Range.locate loc @@ Id s
+  | OPEN_PAREN ->
+    let notations = Combinator.many enotation () in
+    consume Lexer.CLOSE_PAREN;
+    Asai.Range.locate loc @@ L notations
+  | NOTATION_COMMENT ->
+    (* a #; comment will ignore next enotation, and take the next next enotaion *)
+    let _ = enotation () in
+    enotation ()
+  | CLOSE_PAREN | EOF -> raise (UnexpectedToken { loc })
 ;;
 
-let p_all  () : ENotation.t list =
+let p_all () : ENotation.t list =
   let notations = Combinator.many enotation () in
   Combinator.consume Lexer.EOF;
   notations
@@ -41,7 +35,7 @@ let rec tokens filename lexbuf =
 let parse_channel (filename : string) (ch : in_channel) =
   let lexbuf = Lexing.from_channel ch in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-  Combinator.run (tokens filename lexbuf) p_all 
+  Combinator.run (tokens filename lexbuf) p_all
 ;;
 
 let parse_file (filename : string) =
