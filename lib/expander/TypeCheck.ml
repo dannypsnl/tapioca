44922b1e1081e8c9bfae060e034269a71cbcf5c0
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-02-08 22:04:51 +0800

    push location
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- lib/expander/TypeCheck.ml
+++ lib/expander/TypeCheck.ml
@@ -1,22 +1,20 @@
 open Ast
 open Core
 
-let rec check (ctx : Context.t) (tm : Ast.term) (expected : Core.typ) : unit =
+let rec check ~loc (ctx : Context.t) (tm : Ast.term) (expected : Core.typ) : unit =
   match tm, expected with
-  | WithLoc { value; loc }, expected ->
-    Reporter.trace ~loc:(Option.get loc) "term location"
-    @@ fun () -> check ctx value expected
+  | WithLoc { value; loc }, expected -> check ~loc ctx value expected
   | Lambda (params, body), Func (params_ty, ret_ty) ->
     let ctx = Context.create (Some ctx) in
     List.iter2 (fun p ty -> Context.insert ctx p ty) params params_ty;
-    check ctx body ret_ty
+    check ~loc ctx body ret_ty
   | _, _ ->
-    let actual = infer ctx tm in
-    unify ~actual ~expected
+    let actual = infer ~loc ctx tm in
+    unify ~loc ~actual ~expected
 
-and infer (ctx : Context.t) (tm : Ast.term) : Core.typ =
+and infer ~loc (ctx : Context.t) (tm : Ast.term) : Core.typ =
   match tm with
-  | WithLoc { value; _ } -> infer ctx value
+  | WithLoc { value; loc } -> infer ~loc ctx value
   | Int _ -> Int
   | Rational _ -> Rational
   | Float _ -> Float
@@ -27,26 +25,34 @@ and infer (ctx : Context.t) (tm : Ast.term) : Core.typ =
     let new_ctx = Context.create (Some ctx) in
     List.iter
       (fun (name, tm) ->
-         let ty = infer ctx tm in
+         let ty = infer ~loc ctx tm in
          Context.insert new_ctx name ty)
       bs;
-    infer new_ctx t
+    infer ~loc new_ctx t
   | Begin ts ->
     let t = List.hd @@ List.rev ts in
-    infer ctx t
+    infer ~loc ctx t
   | Lambda _ -> Reporter.fatalf Type_error "cannot infer lambda"
   | _ -> Reporter.fatalf TODO "TODO %s" ([%show: Ast.term] tm)
 
-and unify ~(actual : Core.typ) ~(expected : Core.typ) : unit =
+and unify ~loc ~(actual : Core.typ) ~(expected : Core.typ) : unit =
   match actual, expected with
   | Int, Int -> ()
-  | _, _ -> Reporter.fatalf TODO "TODO"
+  | Float, Float -> ()
+  | Int, Float -> ()
+  | _, _ ->
+    Reporter.fatalf
+      ~loc:(Option.get loc)
+      Type_error
+      "expected: %s, got: %s"
+      ([%show: typ] expected)
+      ([%show: typ] actual)
 ;;
 
 let check_module (m : Expander.tapi_module) : unit =
   Hashtbl.iter
     (fun name tm ->
        let ty = Context.lookup m.context name in
-       check m.context tm ty)
+       check ~loc:None m.context tm ty)
     m.tops
 ;;
